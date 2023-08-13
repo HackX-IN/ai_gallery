8cee40481a3908e4b48854038ab465e45d9bb4bd
shivillatech1 <138901497+shivillatech1@users.noreply.github.com>
2023-08-13 11:38:55 +0530

    New

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Lato-Re": require("./src/assets/fonts/Lato-Regular.ttf"),
+    SEA: require("./src/assets/fonts/SEASRN__.ttf"),
+    Italic: require("./src/assets/fonts/LeagueGothic-Italic.otf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
