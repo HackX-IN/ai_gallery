6a464af14ac28802e3873933b0dc6e118b38092a
INAMUL HASAN <114344918+HackX-IN@users.noreply.github.com>
2023-08-19 20:24:28 +0530

    Update GetApi.js

--- src/utils/GetApi.js
+++ src/utils/GetApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://bardapi-kjfc.onrender.com/generate-message";
+const BASE_URL = "Your_Server/generate-message";
 
 const getBardApi = (userMsg) => axios.get(BASE_URL + "?ques=" + userMsg);
 
